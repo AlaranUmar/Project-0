cc27443a00ca5cb318b6d22a09e78780a948a80c
AlaranUmar <alaranumar@gmail.com>
2026-03-11 17:42:21 +0100

    fix: correct import path for Router component

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./App.css";
 import { AuthProvider } from "./context/AuthContext";
-import Router from "./app/router";
+import Router from "./app/Router";
 import { CartProvider } from "./context/CartContext";
 
 createRoot(document.getElementById("root")).render(
